7630c242e2cd5ad3f6137b1e8a71586acc8b3c54
Joel1091 <joelsf43@gmail.com>
2023-12-15 14:12:21 +0100

    Datos pulidos
    He sacado y limpiado los datos sobre: IPC (tanto el indice total como la rama de vivienda), y los salarios por Edades y por nivel de educación

--- Import-joel.R
+++ Import-joel.R
@@ -1,17 +1,12 @@
 #Paquetes preliminares----
 
-install.packages("pxR")
-install.packages()
-library(pxR)
-install.packages("tidyverse")
 library(tidyverse)
-library(data.table)
-library(rlist)
 library(fs)
-library(curl)
 library(rio)
 
 
+#----
+#----
 #IMPORTAR DATOS CE / EUROSTAT----
 options(scipen = 999) #- para quitar la notación científica
 
@@ -58,33 +53,134 @@ df <- df |>
 df <- df |> 
   select(-c(unit_code, values_code, unit, time, na_item_code, na_item, flags))
 
-df <- df %>% mutate(year =  as.numeric(year))
+df <- df |>  mutate(year =  as.numeric(year))
 
-df <- df %>% mutate(iso_2_code =  eurostat::harmonize_country_code(geo_code))
+df <- df |>  mutate(iso_2_code =  eurostat::harmonize_country_code(geo_code))
 
 df <- df |> 
   select(-c(geo_code, flags_code))
 
 library(fs)
-dir_create("datos_pulidos")
+dir_create("datos_pulidos") 
 
 #A continuación vamos a exportar los datos para luego importarlas más fácilmente #library(rio)
 
 export(df, "./datos_pulidos/PIB_pc.csv", type = "csv")
 
-#IMORTAR DATOS INE----
-#Creamos una carpeta para guardar los datos
+#----
+#----
+#IMPORTAR DATOS FRED / MALOS, RENTA----
+df_FRED <- import("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=off&txtcolor=%23444444&ts=12&tts=12&width=718&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=PCAGDPESA646NWDB,NYGDPPCAPKDUSA&scale=left,left&cosd=1960-01-01,1960-01-01&coed=2022-01-01,2022-01-01&line_color=%234572a7,%23aa4643&link_values=false,false&line_style=solid,solid&mark_type=none,none&mw=3,3&lw=2,2&ost=-99999,-99999&oet=99999,99999&mma=0,0&fml=a,a&fq=Annual,Annual&fam=avg,avg&fgst=lin,lin&fgsnd=2020-02-01,2020-02-01&line_index=1,2&transformation=lin,lin&vintage_date=2023-12-14,2023-12-14&revision_date=2023-12-14,2023-12-14&nd=1960-01-01,1960-01-01")
 
-dir_create("./docs/datos")
 
-#Descargamos los datos en formato PX desde el Ine
-#Índice de Precios de Vivienda por CCAA: general, vivienda nueva y de segunda mano
-# Como la URL es:https://www.ine.es/jaxiT3/files/t/es/px/25171.px?nocab=1
+df_long <- gather(df_FRED, key = "Country", value = "PIB_pc", -DATE) |> 
+  rename(year = DATE)
 
-#url <- "https://www.ine.es/jaxiT3/files/t/es/px/25171.px?nocab=1"
-#aa <- read.px(url)
+#ARREGLAR EL FORMATO DE YEAR
 
-#Lo convertimos a CSV para simplificar y creamos un dataframe
+df_long$year <- substr(df_long$year, 1, 4)
 
-#write.csv(aa, file = "data.csv", sep = ",")
-df <- read.csv("data.csv")
+#Arreglamos los nombres de los paises
+
+df_done <- df_long |> 
+  mutate(Country = str_replace(Country, "PCAGDPESA646NWDB", "España")) |> 
+  mutate(Country = str_replace(Country, "NYGDPPCAPKDUSA", "Estados_Unidos")) |> 
+  mutate(year =  as.numeric(year))
+  
+
+
+
+
+#export(df_FRED, "./datos_pulidos/PIB_pc_FRED.csv", type = "csv")
+
+#----
+#----
+#IMPORTAR DATOS INE + LIMPIEZA----
+
+#DATOS SOBRE EL SALARIO POR NIVEL DE FORMACIÓN
+
+library(rio)
+library(tidyverse)
+url <- "https://www.ine.es/jaxiT3/files/t/es/csv_bdsc/13931.csv?nocab=1"
+df_ine <- import(url)
+df_ine_01 <- df_ine |> 
+  filter(Decil %in% ("Total decil")) |> 
+  select("Periodo", "Tipo de jornada", "NIVELES DE FORMACION", "Total") |> 
+  rename(year = Periodo) |> 
+  rename(jornada = 'Tipo de jornada') |> 
+  rename(formacion = 'NIVELES DE FORMACION') |>
+  rename(salario = Total)
+    
+export(df_ine_01,"./datos_pulidos/salario_formacion.csv", type = "csv")
+
+#
+#
+#
+
+#DATOS SOBRE EL SALARIO POR EDADES
+
+url_1 <- "https://www.ine.es/jaxiT3/files/t/es/csv_bdsc/13928.csv?nocab=1"
+df_ine_x1 <- import(url_1)
+
+df_ine_02 <- df_ine_x1 |> 
+  filter(Decil %in% ("Total decil")) |>
+  select("Periodo", "Tipo de jornada", "Grupo de edad", "Total") |> 
+  rename(year = Periodo) |> 
+  rename(jornada = 'Tipo de jornada') |> 
+  rename(grupo_edad = 'Grupo de edad') |>
+  rename(salario = Total)
+  
+export(df_ine_02,"./datos_pulidos/salario_grupo_edad.csv", type = "csv")
+
+#
+#
+#
+
+#DATOS SOBRE EL IPC
+
+url_2 <- "https://www.ine.es/jaxiT3/files/t/es/csv_bdsc/50908.csv?nocab=1"
+df_ipc <- import(url_2)
+
+df_ipc_01 <- df_ipc |> 
+  filter(`Grupos ECOICOP` %in% c("Índice general", "04 Vivienda, agua, electricidad, gas y otros combustibles")) |> 
+  select("Periodo", "Grupos ECOICOP", "Tipo de dato", "Total") |> 
+  rename(year = Periodo) |>
+  rename(grupo = 'Grupos ECOICOP') |> 
+  rename(tipo_dato = `Tipo de dato` )
+
+#PARA SEPARAR EN LA VARIABLE YEAR LOS DATOS POR AÑOS Y MESES
+df_sep <- df_ipc_01 |> 
+  separate(col = year, into = c("year", "month"), sep = "M")
+
+df_sep <- df_sep |> 
+  mutate(month = paste0("M", month))
+
+export(df_sep, "./datos_pulidos/IPC_mas_vivienda.csv", type = "csv")
+#----
+#----
+#EMPEZAMOS CON LAS PRUEBAS DE GRÁFICAS -- EUROSTAT-----
+library(tidyverse)
+library(ggplot2)
+
+df_original<-read.csv("./datos_pulidos/PIB_pc.csv")
+
+df <- df_original |>
+  select(-c(iso_2_code))
+
+df <- df |>
+  filter(country %in% unique(df$country[df$PIB_pc > 23000 & df$year == 2015]) & !grepl("^Euro", country))
+
+#Utilizo la función !grepl para eliminar los paises que empiezen por Euro
+
+ggplot(df, aes(x = year, y = PIB_pc, color = country)) +
+  geom_line()+
+  geom_point()
+
+df_spain <- df_original |> 
+  filter(country == "Spain")
+
+p_spain <- ggplot(df_spain, aes(x = year, y = PIB_pc, color = country)) +
+  geom_line()+
+  geom_point()
+p_spain
+
---
+++ Import.R
@@ -0,0 +1,108 @@
+#Paquetes preliminares
+
+install.packages("downloader")
+install.packages("readxl")
+install.packages("pxR")
+library(pxR)
+library(tidyverse)
+library(data.table)
+library(rlist)
+library(fs)
+library(curl)
+library(rio)
+library(readxl)
+library(downloader)
+library(writexl)
+
+
+
+
+#Creamos una carpeta para guardar los datos
+
+dir_create("./docs/datos")
+
+#Descargamos los datos en formato PX desde el Ine
+#Índice de Precios de Vivienda por CCAA: general, vivienda nueva y de segunda mano
+# Como la URL es:https://www.ine.es/jaxiT3/files/t/es/px/25171.px?nocab=1
+
+url <- "https://www.ine.es/jaxiT3/files/t/es/px/25171.px?nocab=1"
+aa <- read.px(url)
+
+#Lo convertimos a CSV para simplificar y creamos un dataframe
+
+write.csv(aa, file = "data.csv", sep = ",")
+IPV <- read.csv("data.csv")
+
+
+rm(list = ls()[!ls() %in% c("IPV")])
+write.csv(IPV,  file = "./docs/datos/ipv.csv")
+
+#Compra-venta de vivienda
+
+ruta_xls <- "https://apps.fomento.gob.es/BoletinOnline2/sedal/34010110.XLS"
+
+download.file(ruta_xls, "datos.xls", mode = "wb")
+
+lista_datos <- lapply(excel_sheets("datos.xls"), function(sheet) {
+  readxl::read_excel("datos.xls", sheet = sheet)
+})
+
+compraventa_2004 <- read.csv("2004,2005,2006,2007,2008.csv")
+compraventa_2009 <- read.csv("2009,2010,2011,2012,2013.csv")
+compraventa_2014 <- read.csv("2014,2015,2016,2017,2018.csv")
+compraventa_2019 <- read.csv("2019,2020,2021,2022,2023.csv")
+
+?dplyr
+
+cv <- bind_cols(compraventa_2009,compraventa_2014)
+cv <- bind_cols(cv,compraventa_2019)
+
+compraventa_2004 <- compraventa_2004[1:nrow(cv), ]
+
+cv <- bind_cols(cv,compraventa_2004)
+
+
+cv <- cv %>%
+  filter(row_number() %in% 6:72)
+cv <- cv %>%
+  filter(!row_number() %in% 2)
+
+rm(list = ls()[!ls() %in% c("IPV", "cv")])
+export(cv, file = "/docs/datos/cv.csv", format = ".csv")
+
+#Renta por edad y sexo
+
+url3 <- "https://www.ine.es/jaxiT3/files/t/es/px/9942.px?nocab=1"
+aa <- read.px(url3)
+
+#Lo convertimos a CSV para simplificar y creamos un dataframe
+
+write.csv(aa, file = "data.csv", sep = ",")
+renta_edad <- read.csv("data.csv")
+
+rm(list = ls()[!ls() %in% c("IPV", "cv", "renta_edad")])
+
+
+#Renta por nacionalidad
+
+url4 <- "https://www.ine.es/jaxiT3/files/t/es/px/9945.px?nocab=1"
+aa <- read.px(url4)
+
+#Lo convertimos a CSV para simplificar y creamos un dataframe
+
+write.csv(aa, file = "data.csv", sep = ",")
+renta_nacionalidad <- read.csv("data.csv")
+
+rm(list = ls()[!ls() %in% c("IPV", "cv", "renta_edad","renta_nacionalidad" )])
+
+#Hogares por regimen de tenencia edad
+
+url4 <- "https://www.ine.es/jaxiT3/files/t/es/px/9994.px?nocab=1"
+aa <- read.px(url4)
+
+#Lo convertimos a CSV para simplificar y creamos un dataframe
+
+write.csv(aa, file = "data.csv", sep = ",")
+vivienda_edad <- read.csv("data.csv")
+
+rm(list = ls()[!ls() %in% c("IPV", "cv", "renta_edad","renta_nacionalidad", "vivienda_edad" )])
